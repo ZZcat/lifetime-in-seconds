ac56faf4781cdb70fc322dd1c09dd0f01146cba8
ZZcat <ZZcat@users.noreply.github.com>
2015-07-23 14:33:27 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+#! /usr/bin/python
+import datetime , time
+#Made by Zach
+while 1:
+   try:
+      print "What is your birthdate?"
+      year = input ("Birth-day year (example 2013): ")
+      month = input ("Birth-day month (example 4): ")
+      day = input ("Birth-day day (example 13): ")
+      hour = input ("Birth-day hour (example 3): ")
+      minute = input ("Birth-day minute (example 34): ")
+      second = input ("Birth-day second (example 54): ")
+      bday = datetime.datetime(year,month,day,hour,minute,second)
+      while 1:
+          ctime = datetime.datetime.now()
+          a = (ctime-bday).total_seconds()
+          print "                      "*9999
+          print "You are ",a," seconds old"
